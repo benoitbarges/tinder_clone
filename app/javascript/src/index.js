0cd0b58f916345d5cf12faf3f88f52801c59231a
benoit barges <benoitbarges@gmail.com>
2020-12-18 11:50:05 +0100

    isntall react

---
+++ app/javascript/src/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import '../../assets/stylesheets/application.scss'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>REACT</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
